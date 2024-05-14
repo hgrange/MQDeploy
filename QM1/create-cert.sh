e88c88cbca0cf1a5f94f63cbefc13d7edadbcef6
hgrange <33756056+hgrange@users.noreply.github.com>
2024-05-14 19:36:02 +0200

    Update create-cert.sh

--- QM1/create-cert.sh
+++ QM1/create-cert.sh
@@ -24,9 +24,9 @@ openssl req -new -nodes -out MQ/tls/mqm.csr -newkey rsa:4096 -keyout MQ/tls/mqm.
 openssl x509 -req -in MQ/tls/app1.csr -CA MQ/tls/ca.crt -CAkey MQ/tls/ca.key -CAcreateserial -out MQ/tls/app1.crt -days 396 -sha512
 openssl x509 -req -in MQ/tls/mqm.csr -CA MQ/tls/ca.crt -CAkey MQ/tls/ca.key -CAcreateserial -out MQ/tls/mqm.crt -days 396 -sha512
 
-openssl pkcs12 -export -in "MQ/tls/app1.crt" -name "app1" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/app1.key" -out "MQ/tls/app1.p12" -passout pass:password
-openssl pkcs12 -export -in "MQ/tls/mqm.crt" -name "mqm" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/mqm.key" -out "MQ/tls/mqm.p12" -passout pass:password
-openssl pkcs12 -export -in "MQ/tls/qm1.crt" -name "qm1" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/qm1.key" -out "MQ/tls/qm1.p12" -passout pass:password
+openssl pkcs12 -export -in "MQ/tls/app1.crt" -name "ibmwebspheremqqm1" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/app1.key" -out "MQ/tls/app1.p12" -passout pass:password
+openssl pkcs12 -export -in "MQ/tls/mqm.crt" -name "ibmwebspheremqqm1" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/mqm.key" -out "MQ/tls/mqm.p12" -passout pass:password
+openssl pkcs12 -export -in "MQ/tls/qm1.crt" -name "ibmwebspheremqqm1" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/qm1.key" -out "MQ/tls/qm1.p12" -passout pass:password
 oc delete secret qm1-app1-tl
 oc create secret generic qm1-app1-tls -n NAMESPACE --type="kubernetes.io/tls" --from-file=tls.key=MQ/tls/app1.key --from-file=tls.crt=MQ/tls/app1.crt --from-file=ca.crt=MQ/tls/ca.crt --from-file=tls.p12=MQ/tls/app1.p12 --from-literal password=password
 oc delete queuemanager qm1
