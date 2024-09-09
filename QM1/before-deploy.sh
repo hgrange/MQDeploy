f05a6054f071bfbdb6807f775f40a8e3784d1410
hgrange <33756056+hgrange@users.noreply.github.com>
2024-09-09 19:00:51 +0200

    Update before-deploy.sh

--- QM1/before-deploy.sh
+++ QM1/before-deploy.sh
@@ -20,8 +20,8 @@ openssl req -new -nodes -out MQ/tls/mqm.csr -newkey rsa:4096 -keyout MQ/tls/mqm.
 openssl x509 -req -in MQ/tls/app1.csr -CA MQ/tls/ca.crt -CAkey MQ/tls/ca.key -CAcreateserial -out MQ/tls/app1.crt -days 396 -sha512
 openssl x509 -req -in MQ/tls/mqm.csr -CA MQ/tls/ca.crt -CAkey MQ/tls/ca.key -CAcreateserial -out MQ/tls/mqm.crt -days 396 -sha512
 
-openssl pkcs12 -export -in "MQ/tls/app1.crt" -name "ibmwebspheremqqm1" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/app1.key" -out "MQ/tls/app1.p12" -passout pass:password
-openssl pkcs12 -export -in "MQ/tls/mqm.crt" -name "ibmwebspheremqqm1" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/mqm.key" -out "MQ/tls/mqm.p12" -passout pass:password
+openssl pkcs12 -export -in "MQ/tls/app1.crt" -name "app1" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/app1.key" -out "MQ/tls/app1.p12" -passout pass:password
+openssl pkcs12 -export -in "MQ/tls/mqm.crt" -name "mqm" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/mqm.key" -out "MQ/tls/mqm.p12" -passout pass:password
 openssl pkcs12 -export -in "MQ/tls/qm1.crt" -name "ibmwebspheremqqm1" -certfile "MQ/tls/ca.crt" -inkey "MQ/tls/qm1.key" -out "MQ/tls/qm1.p12" -passout pass:password
 
 
